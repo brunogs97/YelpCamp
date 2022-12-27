b2b1ee264b3f22b547248b6b529da16641901ce3
Bruno Gomes <gomes_bruno97@hotmail.com>
2022-12-27 10:32:52 -0300

    Página Sing Up finalizada

---
+++ js/testes.js
@@ -0,0 +1,8 @@
+var coment = document.getElementById('description');
+var element = document.getElementById('comentario');
+
+function adicionar(){
+    //console.log(coment.value);
+
+    alert(`${coment.value}`);
+}
