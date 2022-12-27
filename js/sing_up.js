3de81c5afb6d654d7d72cbee3deedcbe691c42d1
Bruno Gomes <gomes_bruno97@hotmail.com>
2022-12-27 14:01:42 -0300

    Site responsivo página inicial (teste)

---
+++ js/sing_up.js
@@ -0,0 +1,14 @@
+// Função para cadastrar usuário
+function singUp() {
+    alert(`Usuario e Senha cadastrado!`)
+}
+
+// Função para adicionar comentário
+function addComment() {
+    alert(`Comentário adicionado!`)
+}
+
+//Função para adicionar acampamentos
+function addCampground(){
+    alert(`Acampamento adicionado!`)
+}
--- js/testes.js
+++
@@ -1,8 +0,0 @@
-var coment = document.getElementById('description');
-var element = document.getElementById('comentario');
-
-function adicionar(){
-    //console.log(coment.value);
-
-    alert(`${coment.value}`);
-}
